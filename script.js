9cc372b5882d76dc799000372587caa7986d25fa
NeftisIsHere <jazmin.irene.vc@protonmail.com>
2025-05-05 16:11:55 -0400

    Creado formulario y lógica

---
+++ script.js
@@ -0,0 +1,34 @@
+const students=[];
+
+const tableBody=document.querySelector("#students_table tbody");
+
+document.getElementById("student_form").addEventListener("submit",function(e){
+    e.preventDefault();
+    
+    const name=document.getElementById("name").value.trim();
+    const last_name=document.getElementById("last_name").value.trim();
+    const grade=document.getElementById("grade").value.trim();
+
+    if(!name || !last_name || isNaN(grade)){
+        alert("Error, datos incorrectos")
+        return
+    };
+
+    // Guardar datos en el array
+
+    const student = (name, last_name, grade);
+    students.push(student);
+    add_students_row(student);
+    this.reset(); 
+});
+
+function add_students_row(student) {
+    const row = document.createElement("tr");
+    
+    row.innerHTML = 
+    `<td>${student.name}</td>
+    <td> ${student.last_name}</td>
+    <td> ${student.grade}</td>`; 
+    
+    tableBody.appendChild(row);
+};

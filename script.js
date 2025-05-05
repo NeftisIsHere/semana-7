766d9f6207afd07bc249b928e53c4e1454f0c459
NeftisIsHere <jazmin.irene.vc@protonmail.com>
2025-05-05 16:30:31 -0400

    Se agregó la fila de fecha

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ document.getElementById("student_form").addEventListener("submit",function(e){
     
     const name=document.getElementById("name").value.trim();
     const last_name=document.getElementById("last_name").value.trim();
+    const date=document.getElementById("date").value.trim();
     const grade=document.getElementById("grade").value.trim();
 
-    if(!name || !last_name || isNaN(grade)){
+    if(!name || !last_name || !date || isNaN(grade)){
         alert("Error, datos incorrectos")
         return
     };
 
     // Guardar datos en el array
 
-    const student = {name, last_name, grade};
+    const student = {name, last_name, date, grade};
     students.push(student);
     add_students_row(student);
     this.reset(); 
@@ -28,6 +29,7 @@ function add_students_row(student) {
     row.innerHTML = 
     `<td>${student.name}</td>
     <td> ${student.last_name}</td>
+    <td> ${student.date}</td>
     <td> ${student.grade}</td>`; 
     
     tableBody.appendChild(row);
